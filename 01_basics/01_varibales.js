c7333dcc923352fb56c15a2f25c706812a04f0d8
Prakharrohra <128243833+Prakharrohra@users.noreply.github.com>
2023-12-30 14:47:47 +0000

    Okay

---
+++ 01_basics/01_varibales.js
@@ -0,0 +1,7 @@
+const accountId=144553
+let accountEmail="Prakhar@gmail.com"
+var accountPassword="12345"
+accountCity="Lucknow"
+// Last 3 are variables
+console.log(accountId);
+console.table([accountEmail,accountPassword,accountCity]);

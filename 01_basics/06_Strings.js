97c9a5268cb82672729b4dc92520153e9ad2c9f5
Prakharrohra <128243833+Prakharrohra@users.noreply.github.com>
2024-02-02 22:27:32 +0530

    Delete 01_basics directory

--- 01_basics/06_Strings.js
+++
@@ -1,6 +0,0 @@
-const st="str";
-const repocount=40;
-console.log(st+repocount+" Value");
-console.log(`This is concatenation ${st} and repo is ${repocount}`);
-const gameName=new String('Prakhar');
-console.log(gameName.__proto__);
--- 01_basics/HeapAndStack.js
+++
@@ -1,4 +0,0 @@
-let variab="PrakharRohra"
-let var2=variab;
-console.log(var2);
-console.log(variab);
